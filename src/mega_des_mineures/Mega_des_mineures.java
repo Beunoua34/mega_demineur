6027b5803755e75b7148c6fe0c4795916ffeb69c
BENOIT  BASSOMPIERRE <benoit.bassompierre@epfedu.fr>
2022-12-09 10:38:23 +0100

    test

---
+++ src/mega_des_mineures/Mega_des_mineures.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mega_des_mineures;
+
+/**
+ *
+ * @author benba
+ */
+public class Mega_des_mineures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //svp faites que ca marche
+    }
+    
+}
