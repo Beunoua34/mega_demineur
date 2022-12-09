9eb41d3882edc5576838b97418ea378335093c08
BENOIT  BASSOMPIERRE <benoit.bassompierre@epfedu.fr>
2022-12-09 11:15:49 +0100

    Creation des classes

---
+++ src/mega_des_mineures/Cellule.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mega_des_mineures;
+
+/**
+ *
+ * @author benba
+ */
+public class Cellule {
+    private boolean presenceBombe=false;
+    private int bombeAutour;
+    private boolean presenceDrapeau=false;
+    
+}
